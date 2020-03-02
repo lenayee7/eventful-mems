eb829391a5862b3a359a309830925d432ec8c7b3
lenayee7 <lenayee07@gmail.com>
2020-03-01 23:03:56 -0800

    Event and Home page draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
 $(function() {
-    $("#loginButton").click(function() {
-        $("#loginModal").modal("show");
+
+    /* Home page carousel */
+    $(".carousel").carousel( {interval: 5000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
     });
+
+    /* Open Login Modal */
+    $('#loginButton').click(function() {
+        $('#loginModal').modal("show");
+    });
+
+    /* Open Create Event Modal */
+    $('#createEventButton').click(function() {
+        $('#eventModal').modal('show');
+    })
+
+    /* Upcoming/Past */
+    $("#myTab a").click(function(e){
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
+    // const element = document.getElementById('datePicker')
+    // const picker = pickadate.create()
+    // pickadate.render(element, picker)
 });
+
+
+
+    
